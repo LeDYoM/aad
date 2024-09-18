1e5615a6581754b166edf9ec8124c97db90d53e5
Ismael González <ledyom@hotmail.com>
2024-09-18 16:53:08 +0200

    Small improvements

--- src/include/th_pool.h
+++ src/include/th_pool.h
@@ -11,44 +11,53 @@
 #include <atomic>
 #include <type_traits>
 
-class ThreadPool {
+class ThreadPool
+{
 public:
-    explicit ThreadPool(size_t numThreads);
+    explicit ThreadPool(const size_t numThreads);
     ~ThreadPool();
 
     // Task Enqueue function
     template <typename F, typename... Args>
-    auto enqueue(F&& f, Args&&... args) -> std::future<typename std::invoke_result_t<F, Args...>> {
+    auto enqueue(F&& f, Args&&... args)
+        -> std::future<typename std::invoke_result_t<F, Args...>>
+    {
         using returnType = typename std::invoke_result_t<F, Args...>;
 
         // Package the task as a future
-        auto task = std::make_shared<std::packaged_task<returnType()>>(std::bind(std::forward<F>(f), std::forward<Args>(args)...));
+        auto task{std::make_shared<std::packaged_task<returnType()>>(
+            std::bind(std::forward<F>(f), std::forward<Args>(args)...))};
 
-        std::future<returnType> res = task->get_future(); // Get future to return to the caller
+        // Get future to return to the caller
+        std::future<returnType> res{task->get_future()};
 
         {
             // Lock the queue to push the new task
-            std::unique_lock<std::mutex> lock(queueMutex);
+            std::unique_lock<std::mutex> lock{queueMutex};
 
             // Don't allow enqueueing after stopping the pool
-            if (stop) {
+            if (stop)
+            {
                 throw std::runtime_error("Enqueue on stopped ThreadPool");
             }
 
-            tasks.emplace([task]() { (*task)(); }); // Add the task to the queue
+            // Add the task to the queue
+
+            tasks.emplace([t = std::move(task)]() { (*t)(); });
         }
 
-        condition.notify_one(); // Notify one worker thread
+        // Notify one worker thread
+        condition.notify_one();
         return res;
     }
 
 private:
-    std::vector<std::thread> workers;           // Threads in the pool
-    std::queue<std::function<void()>> tasks;    // Task queue
+    std::vector<std::thread> workers;         // Threads in the pool
+    std::queue<std::function<void()>> tasks;  // Task queue
 
-    std::mutex queueMutex;                      // Synchronization
-    std::condition_variable condition;          // Condition variable to notify workers
-    std::atomic<bool> stop;                     // To stop all threads
+    std::mutex queueMutex;              // Synchronization
+    std::condition_variable condition;  // Condition variable to notify workers
+    std::atomic<bool> stop;             // To stop all threads
 };
 
 #endif
