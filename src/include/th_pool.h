6daecca9caa5fb8b92cb7116e929095141c9c087
LeDYoM <ledyom@hotmail.com>
2024-09-19 09:53:42 +0200

    Apply format

--- src/include/th_pool.h
+++ src/include/th_pool.h
@@ -20,44 +20,48 @@ public:
     // Task Enqueue function
     template <typename F, typename... Args>
     auto enqueue(F&& f, Args&&... args)
-        -> std::future<typename std::invoke_result_t<F, Args...>>
-    {
-        using returnType = typename std::invoke_result_t<F, Args...>;
-
-        // Package the task as a future
-        auto task{std::make_shared<std::packaged_task<returnType()>>(
-            std::bind(std::forward<F>(f), std::forward<Args>(args)...))};
-
-        // Get future to return to the caller
-        std::future<returnType> res{task->get_future()};
-
-        {
-            // Lock the queue to push the new task
-            std::unique_lock<std::mutex> lock{queueMutex};
-
-            // Don't allow enqueueing after stopping the pool
-            if (stop)
-            {
-                throw std::runtime_error("Enqueue on stopped ThreadPool");
-            }
-
-            // Add the task to the queue
-
-            tasks.emplace([t = std::move(task)]() { (*t)(); });
-        }
-
-        // Notify one worker thread
-        condition.notify_one();
-        return res;
-    }
+        -> std::future<typename std::invoke_result_t<F, Args...>>;
 
 private:
-    std::vector<std::jthread> workers;         // Threads in the pool
+    std::vector<std::jthread> workers;        // Threads in the pool
     std::queue<std::function<void()>> tasks;  // Task queue
 
     std::mutex queueMutex;              // Synchronization
     std::condition_variable condition;  // Condition variable to notify workers
     std::atomic<bool> stop;             // To stop all threads
 };
+
+template <typename F, typename... Args>
+auto ThreadPool::enqueue(F&& f, Args&&... args)
+    -> std::future<typename std::invoke_result_t<F, Args...>>
+{
+    using returnType = typename std::invoke_result_t<F, Args...>;
+
+    // Package the task as a future
+    auto task{std::make_shared<std::packaged_task<returnType()>>(
+        std::bind(std::forward<F>(f), std::forward<Args>(args)...))};
+
+    // Get future to return to the caller
+    std::future<returnType> res{task->get_future()};
+
+    {
+        // Lock the queue to push the new task
+        std::unique_lock<std::mutex> lock{queueMutex};
+
+        // Don't allow enqueueing after stopping the pool
+        if (stop)
+        {
+            throw std::runtime_error("Enqueue on stopped ThreadPool");
+        }
+
+        // Add the task to the queue
+
+        tasks.emplace([t = std::move(task)]() { (*t)(); });
+    }
+
+    // Notify one worker thread
+    condition.notify_one();
+    return res;
+}
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -2,38 +2,42 @@
 #include <chrono>
 #include "include/th_pool.h"
 
-void exampleTask(int id, int sleepTime) {
+void exampleTask(int id, int sleepTime)
+{
     std::this_thread::sleep_for(std::chrono::milliseconds(sleepTime));
     std::cout << "Task " << id << " completed after " << sleepTime << " ms\n";
 }
 
-int main() {
+int main()
+{
     // Create a thread pool with 4 worker threads
     ThreadPool pool(4);
 
     // Enqueue and execute some tasks
-    for (int i = 1; i <= 8; ++i) {
+    for (int i = 1; i <= 8; ++i)
+    {
         pool.enqueue(exampleTask, i, i * 100);
     }
 
-    for (int i = 9; i <= 18; ++i) {
+    for (int i = 9; i <= 18; ++i)
+    {
         pool.enqueue(exampleTask, i, i * 10);
     }
 
     // The thread pool will automatically clean up in its destructor
-    std::this_thread::sleep_for(std::chrono::seconds(2)); // Wait for tasks to finish
+    std::this_thread::sleep_for(
+        std::chrono::seconds(2));  // Wait for tasks to finish
 
     // Enqueue and execute some tasks
-    for (int i = 1; i <= 8; ++i) {
+    for (int i = 1; i <= 8; ++i)
+    {
         pool.enqueue(exampleTask, i, i * 100);
     }
 
-    for (int i = 9; i <= 18; ++i) {
+    for (int i = 9; i <= 18; ++i)
+    {
         pool.enqueue(exampleTask, i, i * 10);
     }
-
-    // The thread pool will automatically clean up in its destructor
-    std::this_thread::sleep_for(std::chrono::seconds(2)); // Wait for tasks to finish
 
     return 0;
 }
